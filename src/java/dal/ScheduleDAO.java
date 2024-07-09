e54d2a1ac838e9c919a8a8e82cf39aeda650c117
LamNV290304 <nguyenvietlam290304@gmail.com>
2024-07-10 01:17:49 +0700

    Init

--- src/java/dal/ScheduleDAO.java
+++ src/java/dal/ScheduleDAO.java
@@ -3,12 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dal;
+import Model.schedule;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
- *  Date: 08/07/2024
+ *  Date: 10/07/2024
  *  Author: Nguyễn Việt Lâm
- *  Purpose: Load Movie
+ *  Purpose: Load Schedule
  */
-public class ScheduleDAO {
+public class ScheduleDAO extends DBContext{
     
+    // Lấy hết thông tin phim từ database
+    public ArrayList<schedule> getAllMovies() {
+        ArrayList<schedule> list = new ArrayList<>();
+        try {
+
+            //dùng câu lệnh sql để truy vấn
+            String sql = "Select * from schedule";
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                schedule schedule = new schedule();
+                //Set các đặc tính vào đối tượng
+                
+
+                //thêm vào trong danh sách
+                list.add(schedule);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null; //nếu có lỗi trả về null
+        }
+        return list; // trả về danh sách nếu không có lỗi
+    }
 }
